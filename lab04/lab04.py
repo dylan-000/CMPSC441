47ae20187dab346b266c17cd4851fc79fbf30c9a
Dylan <dylan.fisher2468@gmail.com>
2025-02-08 14:24:15 -0500

    Tried fixing prompt to no avail. Prompt passes 50% of tests.

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -29,7 +29,7 @@ lab04_params = {
 if __name__ ==  '__main__':
     trader_template_file = 'lab04/lab04_trader_chat.json'
     run_console_chat(template_file = trader_template_file, 
-                     inventory = "['Apple', 'Banana', 'Great Sword']",
+                     inventory = "['iron ore', 'silver ore']",
                      sign = 'Dylan Fisher',
                      end_regex=r'TRADE(.*)DONE')
     pass
